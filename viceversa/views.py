6f050b03c116ef94d7ad0a6f3055d59ac11bfa98
prokann <annaaudia5@gmail.com>
2021-12-20 16:38:40 +0200

    Start project

---
+++ viceversa/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def about(request):
+	return HttpResponse('This is about page')
+
+
+def home(request):
+	return render(request, 'home.html')
+
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	words = user_text.split()
+	number_of_words = len(words)
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text,
+		'reversedtext': reversed_text, 'number_of_words': number_of_words})
